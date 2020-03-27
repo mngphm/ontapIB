7014325f29142caf865dda888475010a13708c49
mngphm <mingp@MINGP-PC.hq.netapp.com>
2020-03-27 11:40:25 +0100

    initial commit

---
+++ cluster-service/src/test/java/com/ontapib/cluster/ClusterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ontapib.cluster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClusterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
