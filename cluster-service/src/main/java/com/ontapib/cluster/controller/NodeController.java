10b0e9280271fd7d02cadcd0541200ba888dc6ff
mngphm <mingp@MINGP-PC.hq.netapp.com>
2020-05-25 20:21:50 +0200

    fixes

--- cluster-service/src/main/java/com/ontapib/cluster/controller/NodeController.java
+++ cluster-service/src/main/java/com/ontapib/cluster/controller/NodeController.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.ontapib.cluster.model.Node;
 import com.ontapib.cluster.service.NodeService;
 
+@RestController
 public class NodeController {
 	@Autowired
 	private NodeService nodeService;
