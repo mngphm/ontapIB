fdb06d797236768e05677d3942f1e6dc6a75e484
mngphm <mingp@MINGP-PC.hq.netapp.com>
2020-05-26 17:05:44 +0200

    fix

--- cluster-service/src/main/java/com/ontapib/cluster/controller/ClusterController.java
+++ cluster-service/src/main/java/com/ontapib/cluster/controller/ClusterController.java
@@ -137,6 +137,7 @@ public class ClusterController {
 			
 
 			saxParser.parse(new InputSource(new StringReader(getASUPnode)), handler);
+			
 			for (Node node : nodeList) {
 				node.setCluster(c);
 				nodeService.createNode(node);
